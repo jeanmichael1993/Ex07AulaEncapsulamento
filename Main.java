b9b44d71c6abf9d314fca2511c072d5bd2c1fd04
Jean Michael Teixeira Da Silva <jeanmichael760@gmail.com>
2020-02-18 01:12:07 +0000

    Alterando para lista

--- Main.java
+++ Main.java
@@ -1,25 +1,30 @@
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Locale;
 import java.util.Scanner;
 
 import exAula.Circle;
 
-public class Main {
+public class Program {
 
 	public static void main(String [] args) {
 		
 		Locale.setDefault(Locale.US);
 		Scanner sc = new Scanner(System.in);
 		
-		Circle c1 = new Circle(3.0);
-		Circle c2 = new Circle(4.0);
-		Circle c3 = new Circle(7.0);
-
-		System.out.println("c1: \n" + c1);
-		System.out.println("c2: \n" + c2);
-		System.out.println("c3: \n" + c3);
+		List<Circle> circle = new ArrayList<>();
 		
+		for(int i=0;i<3;i++) {
+			System.out.printf("Circle %d%n",i+1);
+			double valor = sc.nextDouble();
+			circle.add(new Circle(valor));
+		}
 		
+		for(Circle x: circle) {
+			System.out.println("\n" + x);
+		}
+			
 		sc.close();
 		
 	}
